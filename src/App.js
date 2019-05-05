7bf6be8a1d52f2f337aee463a74979e1cb7c32e8
George621 <384515231@qq.com>
2019-05-05 18:39:26 +0800

    cli 起步 实现双向绑定

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+import Person from './components/Person/Person'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  state = {
+    persons:[
+      {name:'mis wu',count:50},
+      {name:'george',count:30},
+      {name:'hunery',count:22},
+      {name:'ada',count:11},
+    ]
+  }
+  switchHandler = (c) => {
+    this.setState({
+      persons:[
+        {name:c,count:50},
+        {name:'georges',count:320},
+        {name:'hunery',count:221},
+        {name:'ada',count:111},
+      ]
+    })
+  }
+  nameChangeHandler = (event) => {
+    let newval =  event.target.value
+    this.setState({
+      persons:[
+        {name: newval,count:50},
+        {name: 'georges',count:320},
+        {name: 'hunery',count:221},
+        {name: 'ada',count:111},
+      ]
+    })
+  }
+  render(){
+    let persons =  this.state.persons;
+    let personRender = persons.map((item, index)=>{
+      return <Person changed = {this.nameChangeHandler} name = {item.name} key={index} count = {item.count} />
+    })
+    return (
+      <div className="App">
+        <span>testing</span>
+        {/* <button onClick = {()=>{this.switchHandler('ccc')}}>changes</button> */}
+        <button onClick = {this.switchHandler.bind(this,'missu')}>changes</button>
+        { personRender }
+       
+        
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Person/Person.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './index.css';
+
+const person = (props) => {
+  return <div>
+    <p className="Person">大家好我是{props.name} 我的作品{props.count}</p>
+    <p>{props.children}</p>
+   {/* 双向绑定 next */}
+    <input onChange = {props.changed} type="text" defaultValue = {props.name}/>
+  </div>
+}
+export default person;
